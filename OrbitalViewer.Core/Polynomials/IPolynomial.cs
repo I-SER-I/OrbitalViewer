1326972cd90f1bed3bc2957ca0d2aff8b8db26db
I_SER_I <I_SER_I@hotmail.com>
2021-04-30 20:49:08 +0300

    feat: add laguerre polynomial

---
+++ OrbitalViewer.Core/Polynomials/IPolynomial.cs
@@ -0,0 +1,7 @@
+﻿namespace OrbitalViewer.Core.Polynomials
+{
+    public interface IPolynomial
+    {
+        double GetElement(double firstParameter, double secondParameter, double argument);
+    }
+}
---
+++ OrbitalViewer.Core/Polynomials/LaguerrePolynomial.cs
@@ -0,0 +1,16 @@
+﻿namespace OrbitalViewer.Core.Polynomials
+{
+    public class LaguerrePolynomial : IPolynomial
+    {
+        public double GetElement(double alpha, double k, double argument)
+        {
+            return k switch
+            {
+                0 => 1,
+                1 => 1 + alpha - argument,
+                _ => ((2 * (k - 1) + 1 + alpha - argument) * GetElement(alpha, k - 1, argument) -
+                      (k - 1 + alpha) * GetElement(alpha, k - 2, argument)) / k
+            };
+        }
+    }
+}

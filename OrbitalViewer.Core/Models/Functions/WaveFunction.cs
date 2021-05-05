d41b98b1174ab981af1a28e610ccc654b2e7e0bb
I_SER_I <I_SER_I@hotmail.com>
2021-05-05 23:43:31 +0300

    feat: add wave function

---
+++ OrbitalViewer.Core/Models/Functions/WaveFunction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OrbitalViewer.Core.Models.Functions
+{
+    public class WaveFunction
+    {
+        private readonly RadialFunction _radialFunction;
+        private readonly SphericalFunction _specialFunctions;
+
+        public WaveFunction(int principalQuantumNumber, int azimuthalQuantumNumber, int magneticQuantumNumber)
+        {
+            _radialFunction = new RadialFunction(principalQuantumNumber, azimuthalQuantumNumber);
+            _specialFunctions = new SphericalFunction(azimuthalQuantumNumber, magneticQuantumNumber);
+        }
+
+        public double GetValue(double radius, double theta, double phi) =>
+            Math.Pow(_radialFunction.GetValue(radius) * _specialFunctions.GetValue(theta, phi), 2);
+    }
+}

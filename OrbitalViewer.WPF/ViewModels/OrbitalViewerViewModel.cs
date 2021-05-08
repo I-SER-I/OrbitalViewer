91f31d5cad3e13215f3900f122a9326e70e595c4
I_SER_I <I_SER_I@hotmail.com>
2021-05-08 20:50:17 +0300

    feat: add result info

--- OrbitalViewer.WPF/ViewModels/OrbitalViewerViewModel.cs
+++ OrbitalViewer.WPF/ViewModels/OrbitalViewerViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.Windows.Input;
 using Microsoft.Toolkit.Mvvm.Input;
 using OrbitalViewer.Core.Models.Functions;
 using OrbitalViewer.WPF.Models;
@@ -10,19 +11,26 @@ namespace OrbitalViewer.WPF.ViewModels
     public class OrbitalViewerViewModel : BaseViewModel
     {
         private PlotModel _scatterModel;
-
+        private readonly string[] _letters = {"s", "p", "d", "f", "g", "h", "i", "j", "k"};
         private int _principalQuantumNumber;
         private int _orbitalQuantumNumber;
         private int _magneticQuantumNumber;
+        private string _orbitalsNumber;
+        private string _orbitalName;
+        private string _electronsNumber;
+
+        private bool IsQuantumNumberCorrect() =>
+            _principalQuantumNumber >= 1 && _orbitalQuantumNumber >= 0 &&
+            _orbitalQuantumNumber <= _principalQuantumNumber - 1 &&
+            Math.Abs(_magneticQuantumNumber) <= _orbitalQuantumNumber;
 
         public string PrincipalQuantumNumber
         {
             set
             {
-                int newValue = int.Parse(value);
-                if (_principalQuantumNumber != newValue)
+                if (int.TryParse(value, out int quantumNumber) && _principalQuantumNumber != quantumNumber)
                 {
-                    _principalQuantumNumber = newValue;
+                    _principalQuantumNumber = quantumNumber;
                     OnPropertyChanged();
                 }
             }
@@ -32,10 +40,9 @@ namespace OrbitalViewer.WPF.ViewModels
         {
             set
             {
-                int newValue = int.Parse(value);
-                if (_orbitalQuantumNumber != newValue)
+                if (int.TryParse(value, out int quantumNumber) && _orbitalQuantumNumber != quantumNumber)
                 {
-                    _orbitalQuantumNumber = newValue;
+                    _orbitalQuantumNumber = quantumNumber;
                     OnPropertyChanged();
                 }
             }
@@ -45,10 +52,48 @@ namespace OrbitalViewer.WPF.ViewModels
         {
             set
             {
-                int newValue = int.Parse(value);
-                if (_magneticQuantumNumber != newValue)
+                if (int.TryParse(value, out int quantumNumber) && _magneticQuantumNumber != quantumNumber)
                 {
-                    _magneticQuantumNumber = newValue;
+                    _magneticQuantumNumber = quantumNumber;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string OrbitalsNumber
+        {
+            get => _orbitalsNumber;
+            set
+            {
+                if (_orbitalsNumber != value)
+                {
+                    _orbitalsNumber = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string OrbitalName
+        {
+            get => _orbitalName;
+            set
+            {
+                if (_orbitalName != value)
+                {
+                    _orbitalName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string ElectronsNumber
+        {
+            get => _electronsNumber;
+            set
+            {
+                if (_electronsNumber != value)
+                {
+                    _electronsNumber = value;
                     OnPropertyChanged();
                 }
             }
@@ -73,18 +118,25 @@ namespace OrbitalViewer.WPF.ViewModels
             {
                 return new RelayCommand(() =>
                 {
-                    PlotModel plotModel = new PlotModel
+                    if (IsQuantumNumberCorrect())
                     {
-                        Title = "Electrons probability density"
-                    };
-                    plotModel.Axes.Add(new LinearColorAxis
-                    {
-                        Palette = OxyPalettes.Jet(1024)
-                    });
-                    var waveFunction = new WaveFunction(_principalQuantumNumber, _orbitalQuantumNumber, _magneticQuantumNumber);
-                    var heatMapPlot = new OrbitalViewerPlotModel(waveFunction).GetHeatMapPlot(1000);
-                    plotModel.Series.Add(heatMapPlot);
-                    ScatterModel = plotModel;
+                        PlotModel plotModel = new PlotModel
+                        {
+                            Title = "Electrons probability density"
+                        };
+                        plotModel.Axes.Add(new LinearColorAxis
+                        {
+                            Palette = OxyPalettes.Jet(1024)
+                        });
+                        var waveFunction = new WaveFunction(_principalQuantumNumber, _orbitalQuantumNumber,
+                            _magneticQuantumNumber);
+                        var heatMapPlot = new OrbitalViewerPlotModel(waveFunction).GetHeatMapPlot(1000);
+                        plotModel.Series.Add(heatMapPlot);
+                        ScatterModel = plotModel;
+                        OrbitalsNumber = $"{2 * _orbitalQuantumNumber + 1}";
+                        OrbitalName = $"{_principalQuantumNumber}{_letters[_orbitalQuantumNumber]}";
+                        ElectronsNumber = $"{(2 * _orbitalQuantumNumber + 1) * 2}";
+                    }
                 });
             }
         }

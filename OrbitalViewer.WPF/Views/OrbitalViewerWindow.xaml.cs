3fdbf1d307c8d664097ff7f4aebc38912a03c629
I_SER_I <I_SER_I@hotmail.com>
2021-05-08 20:52:19 +0300

    refactor: remove useless using

--- OrbitalViewer.WPF/Views/OrbitalViewerWindow.xaml.cs
+++ OrbitalViewer.WPF/Views/OrbitalViewerWindow.xaml.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Windows;
-using OrbitalViewer.WPF.ViewModels;
-
-namespace OrbitalViewer.WPF.Views
+﻿namespace OrbitalViewer.WPF.Views
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class OrbitalViewerWindow
     {
         public OrbitalViewerWindow()
